36437cbf752f601c24f90bb0da6d053faf25f4e7
Augustvilliam <willyp@hotmail.se>
2025-11-07 15:09:51 +0100

    finished Phone resulutions.

---
+++ UmbracoAssignment/wwwroot/js/navmenu.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(".phone-nav button");
+    const phoneNav = document.querySelector(".nav-phone");
+
+    if (menuButton && phoneNav) {
+        menuButton.addEventListener("click", () => {
+            phoneNav.classList.toggle("open");
+        });
+    }
+});
